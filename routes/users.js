1bdcae1106e56dc82c3efa9d1c5fe51eea1a1f5f
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-07-23 18:13:28 +0300

    Created User model

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var User = require(`../models/User`);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+  res.json({
+    params : req.params
+  });
 });
 
 module.exports = router;
