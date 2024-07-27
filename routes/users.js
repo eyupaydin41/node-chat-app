b2b7d772242e7b3008a1cd979d9ea4a3ee37a335
Eyüp Aydın <eyup.aydin@istun.edu.tr>
2024-07-28 02:36:30 +0300

    Added user get and delete endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,53 @@
 var express = require('express');
 var router = express.Router();
 var User = require(`../models/User`);
+const Response = require('../lib/Response');
+const bcrypt = require('bcryptjs');
+const { HTTP_CODES } = require('../config/Enum');
+const CustomError = require('../lib/CustomError');
 
-/* GET users listing. */
-router.get('/:id', function(req, res, next) {
-  res.json({
-    params : req.params
-  });
+router.get('/', async (req, res) => {
+    try {
+        let users = await User.find({});
+
+        let successResponse = Response.successResponse(users,HTTP_CODES.OK); 
+        res.status(successResponse.code).json(successResponse);
+    } catch (err) {
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(errorResponse);
+    }
 });
+
+router.get(`/:id`, async (req,res) => {
+    try {
+        let user = await User.findById(req.params.id)
+
+        if (!user) throw new CustomError(HTTP_CODES.NOT_FOUND, `User Not Found`,`Not found user with this id: ${req.params.id}`);
+
+        let successResponse = Response.successResponse(user);
+        res.status(successResponse.code).json(successResponse);
+    } catch (err) {
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(errorResponse);
+    }
+})
+
+
+
+router.delete(`/:id`, async (req,res) => {
+    try {
+        let user = await User.findById(req.params.id)
+
+        if (!user) throw new CustomError(HTTP_CODES.NOT_FOUND, `User Not Found`,`Not found user with this id: ${req.params.id}`);
+
+        await User.deleteOne({ _id: req.params.id });
+
+        let successResponse = Response.successResponse({ success: true });
+        res.status(successResponse.code).json(successResponse);
+    } catch (err) {
+        let errorResponse = Response.errorResponse(err);
+        res.status(errorResponse.code).json(errorResponse);
+    }
+})
 
 module.exports = router;
